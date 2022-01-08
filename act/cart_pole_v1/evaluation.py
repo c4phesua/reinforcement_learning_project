256abd5ee63f41457eab3cbf3655b2b4650c5456
hoang.nguyenthanh <th1475369@gmail.com>
2022-01-08 13:34:29 +0700

    change to logging

--- act/cart_pole_v1/evaluation.py
+++ act/cart_pole_v1/evaluation.py
@@ -1,3 +1,4 @@
+import logging
 import time
 
 import gym
@@ -8,6 +9,10 @@ from tensorflow.keras.layers import Dense
 from act.cart_pole_v1.constant import DATABASE_NAME, TABLE_NAME
 from src.dqn.dqn_model import DQN
 from src.utils.sqlite_utils import get_not_evaluate_latest_weight, create_connection, update_data
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+
 
 if __name__ == '__main__':
     sleep_time = 0.2
@@ -26,7 +31,7 @@ if __name__ == '__main__':
             sleep_time = 0.2
             weight_file = last_save.weight_file
             test_scores = list()
-            print('start testing')
+            logging.debug('---------start testing--------')
             agent.training_network.load_weights(weight_file)
             for i in range(episode):
                 obs = env.reset()
@@ -40,7 +45,7 @@ if __name__ == '__main__':
             average_score = sum(test_scores) / episode
             update_data(last_save.id, average_score, db_conn, table_name=TABLE_NAME)
         else:
-            print('------waiting for new save-------')
+            logging.debug('------waiting for new save-------')
             time.sleep(sleep_time)
             sleep_time = min(sleep_time * 2, 2.5)
         db_conn.close()
