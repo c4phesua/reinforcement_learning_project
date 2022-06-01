42ea89eecb80dc09ec76265f3f7cd9640fee2c31
hoang.nguyenthanh <th1475369@gmail.com>
2022-06-01 20:09:41 +0700

    update evaluate function

--- act/mountainCar_v0/evaluation.py
+++ act/mountainCar_v0/evaluation.py
@@ -6,21 +6,24 @@ import gym
 from tensorflow.keras import optimizers, losses
 from tensorflow.keras.layers import Dense
 
-from act.mountainCar_v0.constant import EVALUATION_QUEUE_NAME
+from act.mountainCar_v0.constant import EVALUATION_QUEUE_NAME, PROFILE_NAME
 from src.dqn.dqn_model import DQN
+from src.utils.file_utils import load_json_file
+from src.utils.mongodb_utils import insert_evaluation_record
 from src.utils.rabbitmq_utils import start_consumer
 
 env = gym.make('MountainCar-v0')
+configs = load_json_file('configs.json')
 
 
 def evaluation_function(ch, method, properties, body):
     json_body = json.loads(body)
-    agent = DQN(0.9, 1, 200, 70000)
-    optimizer = optimizers.RMSprop(learning_rate=0.0001, rho=0.99)
+    agent = DQN(configs['discount_factor'], configs['epsilon'], configs['e_min'], configs['e_max'])
+    optimizer_b = optimizers.Adam(learning_rate=configs['optimizer']['learning_rate'])
     agent.training_network.add(Dense(64, activation='elu', input_shape=(2,)))
     agent.training_network.add(Dense(32, activation='elu'))
-    agent.training_network.add(Dense(3, activation='linear'))
-    agent.training_network.compile(optimizer=optimizer, loss=losses.Huber(delta=1))
+    agent.training_network.add(Dense(3, activation='elu'))
+    agent.training_network.compile(optimizer=optimizer_b, loss=losses.Huber(delta=configs['loss_func']['delta']))
     agent.training_network.set_weights(pickle.loads(bytes.fromhex(json_body['model_weights'])))
     logging.debug('----------episode {}------------'.format(json_body['episode']))
     avg_score = 0
@@ -37,6 +40,8 @@ def evaluation_function(ch, method, properties, body):
                 # env.render()
             avg_score += score / 10.0
         logging.debug(f'------avg score = {avg_score}-----------')
+        insert_evaluation_record(PROFILE_NAME, json_body['batch_id'], json_body['episode'], json_body['model_weights'],
+                                 avg_score)
         ch.basic_ack(delivery_tag=method.delivery_tag)
     except Exception as ex:
         logging.exception(ex)
